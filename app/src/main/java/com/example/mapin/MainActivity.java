24345dee812545a40b1f2e43366b15e5a77d3176
nanashi <nanashi@gmail.com>
2019-12-06 14:30:52 +0900

    new commit

--- app/src/main/java/com/example/mapin/MainActivity.java
+++ app/src/main/java/com/example/mapin/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.evaluation_post_screen);
 
         //米田
         //小熊
