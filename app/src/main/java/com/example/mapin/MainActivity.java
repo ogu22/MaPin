3cdf9e3791f552a8535de4b89238a83d0adb472a
ogu22 <s.oguma22@gmail.com>
2019-12-09 11:13:26 +0900

    リファクタリング

--- app/src/main/java/com/example/mapin/MainActivity.java
+++ app/src/main/java/com/example/mapin/MainActivity.java
@@ -1,20 +1,14 @@
 package com.example.mapin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import static com.example.mapin.R.layout.activity_historysearch;
-import static com.example.mapin.R.layout.activity_main;
-import static com.example.mapin.R.layout.activity_pininfo;
-import static com.example.mapin.R.layout.activity_postdetails;
-import static com.example.mapin.R.layout.activity_search;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profilchange);
+        setContentView(R.layout.login);
     }
 }
